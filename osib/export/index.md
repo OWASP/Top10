99b7a52187ad5fd4fc2217443342d6f050dbe0d0
Florian Schmid <fschmid@schutzwerk.com>
2024-05-21 15:28:54 +0200

    Removed files of older osib package

--- osib/export/index.md
+++
@@ -1,3 +0,0 @@
-# Export
-
-This folder will collect your exported OSIB YAML files when you run mkdocs: 'osib_export_<yyyymmdd>-<hhmmss>.yml'.
--- osib/mkdocsMacroOsib/init__.py
+++
@@ -1,5 +0,0 @@
-# __init__.py
-from .mkdocs_macro_osib import define_env
-from .mkdocs_macro_osib import on_post_build
-
-#provides mkdocs macros 'osib_anchor' and 'osib_link'
